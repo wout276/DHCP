be7aa00e139b6dc5b23ac5c07ecdc92993b44c8f
GillesMoris <gilles.moris@outlook.be>
2016-03-25 00:21:54 +0100

    Waar is clienthandler?

---
+++ src/DHCPclienthandler.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author r0449276
+ *
+ */
+public class DHCPclienthandler implements Runnable {
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		// Dit is de method die wordt aangeroepen bij het starten van de thread, dus de zogezegde main method
+		// de handler moet een field hebben waarin hij zijn controlerende server bijhoud zodat die gegevens aan de server
+		// kan doorgeven (zoals wanneer een client disconnect of hernieuwt.
+
+	}
+
+}
